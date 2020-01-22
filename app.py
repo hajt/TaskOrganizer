05aff27e1d996c104fa45cf4d69e1b496b9fb27c
Rafal <hajtpl@gmail.com>
2020-01-22 19:38:03 +0100

    Working base app

---
+++ app.py
@@ -0,0 +1,66 @@
+"""app.py"""
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_DIR = os.path.join(BASE_DIR, "database.db")
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+DATABASE_DIR
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    is_done = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return "<Task(title='%s')>" % (self.title)
+
+db.create_all()
+
+
+@app.route('/')
+def index():
+    completed_tasks = Task.query.filter_by(is_done=True)
+    incompleted_tasks = Task.query.filter_by(is_done=False)
+    return render_template('index.html', completed_tasks=completed_tasks, incompleted_tasks=incompleted_tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form['task']
+    task = Task(title=title)
+    db.session.add(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<id>')
+def delete(id):
+    # Task.query.filter_by(id=int(id)).delete()
+    # Task.delete()
+    task = Task.query.filter_by(id=int(id)).first()
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/done/<id>')
+def done(id):
+    task = Task.query.filter_by(id=int(id)).first()
+    task.is_done = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/undone/<id>')
+def undone(id):
+    task = Task.query.filter_by(id=int(id)).first()
+    task.is_done = False
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
