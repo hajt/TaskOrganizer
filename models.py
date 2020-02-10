ae29240e2d0fed68a55e802b09a81625c357c78b
Rafal <hajtpl@gmail.com>
2020-02-10 02:19:35 +0100

    FIXUP

--- models.py
+++ models.py
@@ -10,7 +10,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
     email = db.Column(db.String(20))
-    # tasks = db.relationshit('Task')
     
     def __repr__(self):
         return "<User(username='%s', email='%s')>" % (self.username, self.email)
@@ -25,8 +24,7 @@ class Task(db.Model):
     is_done = db.Column(db.Boolean, default=False)
     is_expired = db.Column(db.Boolean, default=False)
     reminder = db.Column(db.Boolean, default=True)
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+ 
     def __repr__(self):
         return "<Task(title='%s', created_date='%s', done_date='%s', expired_date='%s')>" % (self.title, self.created_date, self.done_date, self.expired_date)
 
